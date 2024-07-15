05f1c9e053ebddacced0d0bff2c89284552422e8
Michal Wesolowski <michal.wesolov@gmail.com>
2024-07-15 21:28:43 +0200

    implemented incomplete navigation with journal

--- App.tsx
+++ App.tsx
@@ -1,27 +1,11 @@
 import { store } from './src/redux/Store'
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
 import { Provider } from 'react-redux'
-import Test from './src/screens/test'
-import Login from './src/screens/auth/Login'
+import AppNavigation from './src/components/appNavigation/AppNavigation'
 
 export default function App() {
   return (
     <Provider store={store}>
-      <Text>{`${process.env.EXPO_PUBLIC_API_URL}/api/token/refresh/`}</Text>
-      <View style={styles.container}>
-        <Login />
-        <StatusBar style="auto" />
-      </View>
+      <AppNavigation />
     </Provider>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
