82a1e3c02a09586cc3db5cd904fb0087d9371490
Michal Wesolowski <michal.wesolov@gmail.com>
2024-07-30 15:19:54 +0200

    updated journal api slice

--- src/redux/api/slices/JournalApiSlice.ts
+++ src/redux/api/slices/JournalApiSlice.ts
@@ -5,12 +5,16 @@ import { Journal, JournalPage } from '../../../types/Types'
 export const journalApiSlice = createApi({
   reducerPath: 'journalApi',
   baseQuery: baseQueryWithReauth,
+  tagTypes: ['Journal'],
   endpoints: builder => ({
     getJournals: builder.query<JournalPage, void>({
-      query: () => `api/journal/`
+      query: () => `api/journal/`,
+      providesTags: result => (result ? [{ type: 'Journal', id: 'LIST' }] : [])
     }),
     getJournalsByDate: builder.query<Journal[], { year: number; month: number; day: number }>({
-      query: ({ year, month, day }) => `api/journal/?year=${year}&month=${month}&day=${day}`
+      query: ({ year, month, day }) => `api/journal/?year=${year}&month=${month}&day=${day}`,
+      providesTags: (result, error, { year, month, day }) =>
+        result ? result.map(({ id }) => ({ type: 'Journal', id: id })) : []
     })
   })
 })
