290e64637a5c9d1b16b792f10d6b4abbc3a8d6cc
Michal Wesolowski <michal.wesolov@gmail.com>
2024-07-31 13:04:43 +0200

    updated product api slice

--- src/redux/api/slices/ProductApiSlice.ts
+++ src/redux/api/slices/ProductApiSlice.ts
@@ -1,12 +1,34 @@
 import { createApi } from '@reduxjs/toolkit/query/react'
-import { baseQueryWithReauth } from '../querys/BaseQueryReauth'
-import { ProductDetails } from '../../../types/Types'
+import { baseQueryWithReauth } from '../queries/BaseQueryReauth'
+import { ProductDetails, ProductPage } from '../../../types/Types'
 
 export const productApiSlice = createApi({
   reducerPath: 'productApi',
   baseQuery: baseQueryWithReauth,
   tagTypes: ['Product', 'Journal'],
   endpoints: builder => ({
+    getProducts: builder.query<ProductPage, { page: number }>({
+      query: ({ page }) => ({
+        url: `api/products?page=${page}`,
+        method: 'GET'
+      }),
+      providesTags: (result, error, { page }) =>
+        result
+          ? [
+              ...result.results.map(({ barcode }) => ({ type: 'Product' as const, id: barcode })),
+              { type: 'Product' as const, id: 'PARTIAL_LIST' },
+              { type: 'Product' as const, id: `PAGE-${page}` }
+            ]
+          : [{ type: 'Product' as const, id: 'PARTIAL_LIST' }]
+    }),
+    addProduct: builder.mutation<ProductDetails, Omit<ProductDetails, 'added_by' | 'url' | 'id'>>({
+      query: product => ({
+        url: 'api/products/',
+        method: 'POST',
+        body: product
+      }),
+      invalidatesTags: [{ type: 'Product', id: 'PARTIAL_LIST' }]
+    }),
     getProduct: builder.query<ProductDetails, ProductDetails['barcode']>({
       query: barcode => `api/product/${barcode}`,
       providesTags: (result, error, barcode) => [{ type: 'Product' as const, id: barcode }]
@@ -17,10 +39,15 @@ export const productApiSlice = createApi({
         method: 'PUT',
         body: product
       }),
-      invalidatesTags: (result, error, { barcode }) => [
-        { type: 'Product', id: barcode },
-        { type: 'Journal', id: 'BY_DATE' }
-      ]
+      invalidatesTags: () => [{ type: 'Journal', id: 'BY_DATE' }],
+      onQueryStarted({ barcode, ...put }, { dispatch, queryFulfilled }) {
+        const putResult = dispatch(
+          productApiSlice.util.updateQueryData('getProduct', barcode, draft => {
+            Object.assign(draft, put)
+          })
+        )
+        queryFulfilled.catch(putResult.undo)
+      }
     }),
     patchProduct: builder.mutation<ProductDetails, Partial<ProductDetails> & Pick<ProductDetails, 'barcode'>>({
       query: product => ({
@@ -28,10 +55,15 @@ export const productApiSlice = createApi({
         method: 'PATCH',
         body: product
       }),
-      invalidatesTags: (result, error, { barcode }) => [
-        { type: 'Product', id: barcode },
-        { type: 'Journal', id: 'BY_DATE' }
-      ]
+      invalidatesTags: () => [{ type: 'Journal', id: 'BY_DATE' }],
+      onQueryStarted({ barcode, ...patch }, { dispatch, queryFulfilled }) {
+        const patchResult = dispatch(
+          productApiSlice.util.updateQueryData('getProduct', barcode, draft => {
+            Object.assign(draft, patch)
+          })
+        )
+        queryFulfilled.catch(patchResult.undo)
+      }
     }),
     deleteProduct: builder.mutation<void, Partial<ProductDetails> & Pick<ProductDetails, 'barcode'>>({
       query: ({ barcode }) => ({
@@ -46,5 +78,12 @@ export const productApiSlice = createApi({
   })
 })
 
-export const { useGetProductQuery, useUpdateProductMutation, usePatchProductMutation, useDeleteProductMutation } =
-  productApiSlice
+export const {
+  useGetProductQuery,
+  useLazyGetProductQuery,
+  useUpdateProductMutation,
+  usePatchProductMutation,
+  useDeleteProductMutation,
+  useAddProductMutation,
+  useGetProductsQuery
+} = productApiSlice
